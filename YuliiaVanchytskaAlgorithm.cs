5465431ff850a43dbaf7a192f4a3a87cb02eea77
juliavan <julia.van2801@gmail.com>
2021-09-20 15:45:59 +0300

    Add project files.

---
+++ YuliiaVanchytskaAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robot.Common;
+
+namespace YuliiaVanchytska.RobotChallenge
+{
+    public class YuliiaVanchytskaAlgorithm : IRobotAlgorithm
+    {
+        public string Author
+        {
+            get { return "Yuliia Vanchytska"; }
+        }
+
+        public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+            var myRobot = robots[robotToMoveIndex];
+            Position stationPosition = null;
+            return new MoveCommand() { NewPosition = stationPosition };
+        }
+    }
+}
